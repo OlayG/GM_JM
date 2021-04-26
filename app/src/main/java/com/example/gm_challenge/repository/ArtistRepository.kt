8d93cbfdecf7d60a70f08756789c7e8376da8a4f
Olay G <olayg@icloud.com>
2021-04-26 11:39:27 -0400

    undo change

--- app/src/main/java/com/example/gm_challenge/repository/ArtistRepository.kt
+++ app/src/main/java/com/example/gm_challenge/repository/ArtistRepository.kt
@@ -3,15 +3,14 @@ package com.example.gm_challenge.repository
 import com.example.gm_challenge.model.Result
 import com.example.gm_challenge.service.ArtistApi
 import com.example.gm_challenge.util.Resource
-import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
-class ArtistRepository @Inject constructor(
+class ArtistRepository  @Inject constructor(
     private val artistApi: ArtistApi
 ) {
 
     suspend fun fetchArtists(artistName: String): Resource<List<Result>> {
-        return if (artistName.isEmpty()) {
+        return if(artistName.isEmpty()) {
             Resource.Error("Enter artist name")
         } else {
             val response = artistApi.fetchArtists(artistName)
@@ -19,17 +18,5 @@ class ArtistRepository @Inject constructor(
                 Resource.Success(it.results)
             } ?: Resource.Error("Your request produces an error")
         }
-    }
-
-    suspend fun fetchArtists2(artistName: String) = flow {
-        val resource = if (artistName.isEmpty()) {
-            Resource.Error("Enter artist name")
-        } else {
-            val response = artistApi.fetchArtists(artistName)
-            response.body()?.let {
-                Resource.Success(it.results)
-            } ?: Resource.Error("Your request produces an error")
-        }
-        emit(resource)
     }
 }
